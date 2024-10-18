410a11c178461c664d51be8b2b9c0a0515c18075
EduardoMendo <efmendoza@uamv.edu.ni>
2024-10-17 18:25:35 -0600

    Visualizacion del primer video

---
+++ src/main/java/org/efmb/facturacion/modelo/Cliente.java
@@ -0,0 +1,21 @@
+package org.efmb.facturacion.modelo;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@Entity
+@Getter @Setter
+public class Cliente {
+	
+	@Id
+	@Column(length = 6)
+	int numero;
+	
+	@Column(length = 50)
+	@Required
+	String nombre;
+
+}
